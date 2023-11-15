e6fa6285ac0af2696533e253e641cb27aead1708
EintsWaveX <nue.immanuel@gmail.com>
2023-11-15 18:18:40 +0700

    No changes nade, only do a little tweaking.

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,4 @@
-Source
+for (int i = 0; i < strlen(InputSex); i++) {
+                if (InputSex[i] != 10 || InputSex[i] != -10 || InputSex[i] != 127) { TempInputSex[i] = toupper((unsigned char)InputSex[i]);
+                } else break;
+            }
