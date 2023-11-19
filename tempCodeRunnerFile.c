91a4f88212eedcb3e2c92deb1f3f9658f4bcc62f
EintsWaveX <nue.immanuel@gmail.com>
2023-11-19 20:26:03 +0700

    Adding interactive UI into login and registration.

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,4 +1,1 @@
-for (int i = 0; i < strlen(InputSex); i++) {
-                if (InputSex[i] != 10 || InputSex[i] != -10 || InputSex[i] != 127) { TempInputSex[i] = toupper((unsigned char)InputSex[i]);
-                } else break;
-            }
+\n\n\n\n
